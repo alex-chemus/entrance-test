9fa30d0d5b516b2762cf1d08a473e7647e9db001
Shura-X <48450240+Shura-X@users.noreply.github.com>
2021-03-27 19:09:06 +0500

    fixed regex
    Changed regex for .form__button and ended working on the project

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const chalk = require('chalk')
 const app = express()
 
 const port = process.env.PORT || 8080
@@ -6,11 +7,9 @@ const port = process.env.PORT || 8080
 app.use( express.static(__dirname) )
 app.get('', (req, res) => {
     res.sendFile('./index.html')
-    //console.log(__dirname + '\\index.html')
-    //console.log('.\\styles\\css')
     res.end()
 })
 
 app.listen(port, () => {
-    console.log('Server has been started')
+    console.log(`Сервер был успешно запущен. Ссылка: ${chalk.blue('localhost:8080')}`)
 })
--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const form = document.querySelector('.form')
 form.addEventListener('submit', event => {
     event.preventDefault()
 
+    //создать пустую форму и заполнить ее теми же полями,
+    //что и .form
     console.log(form)
     const formData = new FormData(form)
     console.log(formData)
     
-    fetch('https://60376bfd5435040017722533.mockapi.io/formRej', {
+    fetch('https://60376bfd5435040017722533.mockapi.io/form', {
         method: 'POST',
         body: formData
     })
     .then(response => { 
+        //проверка статуса ответа
         console.log(response.status)
         if (response.status != 201) {
             return Promise.reject()
